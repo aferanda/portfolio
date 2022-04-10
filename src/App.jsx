1ed6e5718780a919e2f17da7162022c5bf26fb9d
Amanda Fernandes <aferanda@gmail.com>
2022-04-10 16:13:40 -0300

    build: add dependencie

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import GlobalStyle from './styles/global'
+import Header from './components/header/Header'
+import Main from './components/main/Main'
 
 function App() {
   return (
-    <div>
-      Amanda Fernandes
-    </div>
+    <>
+      <GlobalStyle />
+      <Header />
+      <Main />
+    </>
   )
 }
 
