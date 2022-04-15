fe45a3bd55d5df5a2f7577a09a9bd09cd24f6d0e
Amanda Fernandes <aferanda@gmail.com>
2022-04-15 14:51:53 -0300

    feat: update components

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
-import { HeaderStyle } from './style'
+import React, { useRef } from 'react';
+import { useEffect } from 'react/cjs/react.development';
+import { init } from 'ityped';
+import { Container, ImageContainer, TextContainer } from './style';
+
+import image from '../../images/undraw_proud_coder_re_exuy.svg';
 
 export default function Header() {
+
+  const textInput = useRef();
+
+  useEffect(() => {
+    init(textInput.current, {
+      showCursor: true,
+      backDelay: 1100,
+      backSpeed: 40,
+      strings: ["< Desenvolvedora Web />"]
+    })
+  }, [])
+
   return (
-    <HeaderStyle>
-      <nav>
-        <p>Image</p>
-        <p>Get started</p>
-      </nav>
-    </HeaderStyle>
+    <Container
+      initial={{ opacity: 0 }}
+      whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
+      transition={{ duration: 0.5 }}
+      viewport={{ once: true }}
+    >
+      <TextContainer>
+        <h1>Amanda Fernandes</h1>
+        <h2><span ref={textInput}></span></h2>
+        <button>
+          <div>Get started</div>
+        </button>
+      </TextContainer>
+      <ImageContainer>
+        <img src={image} alt="developer woman photo" />
+      </ImageContainer>
+    </Container>
   )
 }
--- src/components/header/style.jsx
+++ src/components/header/style.jsx
@@ -1,14 +1,133 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
-export const HeaderStyle = styled.header`
-  padding: 0 10%;
-  position: fixed;
+export const Container = styled(motion.header)`
+  color: var(--text);
+  min-height: 100vh;
   width: 100%;
+  display: flex;
+  align-items: center;
+  margin: 0px auto;
+  overflow: hidden;
+  padding: 0 10%;
 
-  nav {
-    height: 80px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+  @media (max-width: 900px) {
+    flex-direction: column;
+    height: auto;
   }
 `
+
+export const TextContainer = styled.div`
+  width: 50%;
+
+  @media (max-width: 900px) {
+    margin-top: 100px;
+    padding: 50px 20px;
+    text-align: center;
+    width: 100%;
+  }
+
+  h1 {
+    background-clip: text;
+    background-image: linear-gradient(
+      to right,
+      var(--pink),
+      var(--purple),
+      var(--yellow)
+    );
+    color: var(--text);
+    display: inline;
+    font-family: Montserrat Alternates, monospace, serif;
+    font-weight: 700;
+    font-size: 2.8rem;
+    line-height: 1.2;
+    margin-bottom: var(--spacing-4);
+    padding-right: 5px;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+
+  h2 {
+    font-family: Roboto Mono, sans-serif;
+    font-size: 1.1rem;
+    font-weight: 500;
+    line-height: 1.5;
+    margin-bottom: var(--spacing-4);
+  }
+
+  button {
+    background: var(--white);
+    border-radius: 14px;
+    border: none;
+    box-shadow: 0 0 0 3px var(--border);
+    font-family: 'Montserrat', sans-serif;
+    font-size: 1.1rem;
+    font-weight: 700;
+    height: 50px;
+    padding: 0 20px;
+    position: relative;
+    transition: all 0.2s ease-in-out 0s;
+
+    div {
+      background-image: linear-gradient(
+        to right,
+        var(--pink),
+        var(--purple),
+        var(--yellow)
+      );
+      background-clip: text;
+      color: var(--text);
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+    }
+  }
+
+  button::before {
+    backface-visibility: hidden;
+    background: linear-gradient(
+      -45deg,
+      var(--pink),
+      var(--purple),
+      var(--yellow)
+    );
+    border-radius: 17px;
+    content: '';
+    height: calc(100% + 6px);
+    left: -3px;
+    opacity: 0;
+    position: absolute;
+    top: -3px;
+    transform: translateX(0px);
+    transition: all 0.2s ease-in-out 0s;
+    width: calc(100% + 6px);
+    z-index: -1;
+  }
+
+  button:active {
+    background: var(--lightgrey);
+  }
+
+  button:hover {
+    box-shadow: transparent 0px 0px 0px 2px;
+    cursor: pointer;
+    transition: all 0.2s ease-in-out 0s;
+  }
+
+  button:hover::before {
+    opacity: 1;
+  }
+`;
+
+export const ImageContainer = styled(motion.div)`
+  width: 50%;
+
+  @media (max-width: 900px) {
+    width: 100%;
+    padding: 50px 20px;
+    text-align: center;
+  }
+
+  img {
+    width: 100%;
+  }
+`;
