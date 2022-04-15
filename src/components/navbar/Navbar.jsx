a2385e9f84fdd723679cb02b70a8625927fb1b9b
Amanda Fernandes <aferanda@gmail.com>
2022-04-15 14:52:20 -0300

    feat: create navbar component

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { NavBarStyle } from './style'
+
+export default function NavBar() {
+  return (
+    <NavBarStyle>
+      <p>Image</p>
+      <p>Get started</p>
+    </NavBarStyle>
+  )
+}
---
+++ src/components/navbar/style.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const NavBarStyle = styled.nav`
+  padding: 0 10%;
+  position: fixed;
+  width: 100%;
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
