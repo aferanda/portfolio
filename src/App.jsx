041ca619c43bb2fac0e988d20e4e875bde0ef68b
Amanda Fernandes <aferanda@gmail.com>
2022-04-15 14:52:48 -0300

    feat: update components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import GlobalStyle from './styles/global'
+import NavBar from './components/navbar/Navbar'
 import Header from './components/header/Header'
-import Main from './components/main/Main'
+import About from './components/about/About'
 
 function App() {
   return (
     <>
       <GlobalStyle />
+      <NavBar />
       <Header />
-      <Main />
+      <About />
     </>
   )
 }
