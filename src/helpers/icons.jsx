044171cae35269b6a9110d72cd9627dd6c2c66d8
Amanda Fernandes <aferanda@gmail.com>
2022-04-15 22:48:46 -0300

    style: add icons

---
+++ src/helpers/icons.jsx
@@ -0,0 +1,43 @@
+import html from '../images/icons/html-1.svg';
+import css from '../images/icons/css-3.svg';
+import javascript from '../images/icons/logo-javascript.svg';
+import mysql from '../images/icons/icons8-mysql-logo-512.png';
+import node from '../images/icons/icons8-node-js-512.png';
+import docker from '../images/icons/icons8-docker-512.png';
+import python from '../images/icons/python-5.svg';
+import react from '../images/icons/react-2.svg';
+import redux from '../images/icons/redux.svg';
+import sass from '../images/icons/sass-1.svg';
+import swift from '../images/icons/swift.png';
+import typescript from '../images/icons/typescript.png';
+import styled from '../images/icons/styled-components.png';
+import github from '../images/icons/github-icon-1.svg';
+import express from '../images/icons/express-109.svg';
+import git from '../images/icons/git-icon.svg';
+import mongodb from '../images/icons/mongodb-icon-1.svg';
+import graphql from '../images/icons/graphql.svg';
+import linkedin from '../images/icons/icons8-linkedin-512.png';
+import email from '../images/icons/icons8-mail-512.png';
+
+export default {
+  html,
+  css,
+  javascript,
+  mysql,
+  node,
+  docker,
+  python,
+  react,
+  redux,
+  sass,
+  swift,
+  typescript,
+  styled,
+  github,
+  git,
+  express,
+  mongodb,
+  graphql,
+  linkedin,
+  email,
+}
