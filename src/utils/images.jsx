2f96f0c0e69116098b13fbfaaff105b257fda6c2
Amanda Fernandes <aferanda@gmail.com>
2022-04-22 14:05:28 -0300

    style: organize imagens and icons

---
+++ src/utils/images.jsx
@@ -0,0 +1,11 @@
+import todolist from '../assets/images/todolist.png';
+import trybewallet from '../assets/images/trybewallet.png';
+import trybetunes from '../assets/images/trybetunes.png';
+import tryunfo from '../assets/images/tryunfo.png';
+
+export default {
+  todolist,
+  trybewallet,
+  trybetunes,
+  tryunfo
+}
