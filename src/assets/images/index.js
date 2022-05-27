56f57c89d81d78ebf8f412ab4e9b9a6c89e2056d
Amanda Fernandes <aferanda@gmail.com>
2022-05-27 12:11:36 -0300

    chore: add images

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -5,6 +5,8 @@ import tryunfo from './tryunfo.png';
 import signupform from './sign-up-form.png';
 import imgLight from './undraw_web_development_0l6v-dark.svg';
 import imgDark from './undraw_web_development_0l6v-dark.svg';
+import womanReact from './undraw_react_re_g3ui.svg';
+import womanLove from './undraw_with_love_re_1q3m.svg';
 import contact from './undraw_contact_us_re_4qqt.svg';
 import crud from './crud.png';
 
@@ -18,4 +20,6 @@ export {
   imgLight,
   imgDark,
   crud,
+  womanReact,
+  womanLove,
 }
